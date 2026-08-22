83c8915ecd10049b5c58648a66d91dce8a26cca7
Mochizhouuu <mochizhouuu@gmail.com>
2026-08-22 09:14:44 +0700

    Fix: remove unresolvable pdfbox-android dependency (unused for now)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,5 +88,4 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-    implementation("org.apache.pdfbox:pdfbox-android:2.0.27.0")
 }
